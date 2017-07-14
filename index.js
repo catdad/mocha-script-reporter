3379eeaa31c6dd8e0f48bcd0ab251821095981e0
Kiril Vatev <vatev.1@gmail.com>
2017-07-13 20:24:27 -0400

    adding some more error handling

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 /* jshint node: true */
 
+var util = require('util');
 var path = require('path');
+
+var NO_PATH_OPTION = util.format(
+  'no reporter script path specified, use "%s"',
+  '--reporter-options path=./path/to/file.js'
+);
+var FILE_DOES_NOT_EXIST = 'no file found at "%s"';
 
 module.exports = function ScriptReporter(runner, options) {
   if (!options.reporterOptions || !options.reporterOptions.path) {
-    throw new Error('no reporter script path specified, use "--reporter-options path=./path/to/file.js"');
+    throw new Error(NO_PATH_OPTION);
   }
 
   var reporterPath = path.resolve(options.reporterOptions.path);
+
+  try {
+    require.resolve(reporterPath);
+  } catch (e) {
+    throw new Error(util.format(FILE_DOES_NOT_EXIST, reporterPath));
+  }
+
   var reporter = require(reporterPath);
 
   return new reporter(runner, options);
